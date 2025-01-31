ef05c6c1e49c6d1aa75a8c1dd6e767bfd7b45110
Mikhail Isaev <isaev.mihael@gmail.com>
2025-01-31 21:34:40 +0400

    🪚 Implement `handleRenderRequest` method

---
+++ firebase.js
@@ -0,0 +1,41 @@
+import { setupCloudFunction } from './cloudFunction.js'
+import { fileURLToPath } from 'url'
+import * as fs from 'fs'
+import * as path from 'path'
+
+// Server instance
+var server = undefined
+var config = undefined
+
+const firebaseConfig = (importMetaUrl) => {
+    const __filename = fileURLToPath(importMetaUrl)
+    const __dirname = path.dirname(__filename)
+    const configPath = path.join(__dirname, '../firebase.json')
+    config = JSON.parse(fs.readFileSync(configPath, 'utf8'))
+    if (!config.hosting) throw 'Missing \'hosting\' configuration in ../firebase.json'
+    if (!config.hosting.public || config.hosting.public.trim().length == 0) throw 'Missing \'hosting.public\' value in ../firebase.json'
+    if (!config.hosting.wasm || config.hosting.wasm.trim().length == 0) throw 'Missing \'hosting.wasm\' value in ../firebase.json'
+    if (!config.hosting.index || config.hosting.index.trim().length == 0) throw 'Missing \'hosting.index\' value in ../firebase.json'
+    if (!config.hosting.index.endsWith('.html')) throw '\'hosting.index\' value in ../firebase.json: should end with .html'
+    if (config.hosting.crawlers && config.hosting.crawlers.trim().length > 0) {
+        const t = config.hosting.crawlers.split(',')
+        if (t.length > 0) customBots = t
+    }
+    config.staticFilesDir = path.join(__dirname, `../${config.hosting.public}`)
+}
+
+export const handleRenderRequest = async (importMetaUrl, logger, req, reply, customBots, numberOfChildProcesses) => {
+    if (!config) firebaseConfig(importMetaUrl)
+    if (!server) {
+        server = setupCloudFunction({
+            pathToStaticFiles: config.staticFilesDir,
+            pathToWasm: path.join(config.staticFilesDir, `${config.hosting.wasm}.wasm`),
+            indexFile: config.hosting.index,
+            logger: logger,
+            numberOfChildProcesses: numberOfChildProcesses, // 4 by default
+            customBots: customBots
+        })
+    }
+    await server.fastify.ready()
+    server.fastify.server.emit('request', req, reply)
+}
